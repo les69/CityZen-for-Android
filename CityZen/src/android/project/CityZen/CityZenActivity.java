aebbafec9671304243bec4c09f61147d58a2ac20
les69 <nanotrippolo@gmail.com>
2012-12-01 14:26:04 +0100

    commit-all

---
+++ CityZen/src/android/project/CityZen/CityZenActivity.java
@@ -0,0 +1,13 @@
+package android.project.CityZen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CityZenActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.root);
+    }
+}
