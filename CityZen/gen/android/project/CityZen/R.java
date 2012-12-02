e7191b682076974fe8603d62960e03de5e662ddd
Alessandro Domanico <alessandro.domanico@yahoo.it>
2012-12-02 13:38:38 +0100

    settings

--- CityZen/gen/android/project/CityZen/R.java
+++ CityZen/gen/android/project/CityZen/R.java
@@ -32,10 +32,10 @@ public final class R {
         public static final int image_firstDisplayed=0x7f07000a;
         public static final int img_preview=0x7f070008;
         public static final int item_category=0x7f070001;
+        public static final int mymap=0x7f070005;
         public static final int textView1=0x7f070000;
         public static final int textView2=0x7f070002;
         public static final int txt_description=0x7f070003;
-        public static final int txt_temp=0x7f070005;
     }
     public static final class layout {
         public static final int description=0x7f030000;
@@ -43,6 +43,8 @@ public final class R {
         public static final int map=0x7f030002;
         public static final int picture=0x7f030003;
         public static final int root=0x7f030004;
+        public static final int second_step=0x7f030005;
+        public static final int third_step=0x7f030006;
     }
     public static final class string {
         public static final int BeginButtonText=0x7f050002;
@@ -52,6 +54,7 @@ public final class R {
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
         public static final int logo=0x7f050006;
+        public static final int mapskey=0x7f050008;
         public static final int txtCategory=0x7f050004;
     }
 }
